e5fd890e05edf87aca38240d6265cdacc8ba2b83
emirhanusta <emirhan1usta@gmail.com>
2024-05-24 22:00:19 +0300

    Refactor: config server deleted and vault config added

--- author-service/build.gradle.kts
+++ author-service/build.gradle.kts
@@ -31,7 +31,6 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("io.micrometer:micrometer-tracing-bridge-brave")
 	implementation("io.zipkin.reporter2:zipkin-reporter-brave")
-	implementation("org.springframework.cloud:spring-cloud-starter-config")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 dependencyManagement {
