07394a1d36a11191a8753e5a46f12add6431d67b
emirhanusta <emirhan1usta@gmail.com>
2024-06-09 16:04:19 +0300

    Author service Updated to store a large object

--- author-service/src/main/kotlin/authorservice/model/Author.kt
+++ author-service/src/main/kotlin/authorservice/model/Author.kt
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
+import jakarta.persistence.Lob
 import org.hibernate.annotations.CreationTimestamp
 import org.hibernate.annotations.UpdateTimestamp
 import java.time.LocalDate
@@ -16,6 +17,7 @@ data class Author(
     @GeneratedValue(strategy = GenerationType.UUID)
     val id: UUID? = null,
     var name: String,
+    @Lob
     var biography: String,
     var birthDate: LocalDate,
     @CreationTimestamp
